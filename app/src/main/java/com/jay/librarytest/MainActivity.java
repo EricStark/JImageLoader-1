b4c22fd52c1bad7e0dd3907e5aff77395504b7e0
Jay <1432562823@qq.com>
2017-03-17 19:16:24 +0800

    first commit

--- app/src/main/java/com/jay/librarytest/MainActivity.java
+++ app/src/main/java/com/jay/librarytest/MainActivity.java
@@ -2,23 +2,11 @@ package com.jay.librarytest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-
-import com.jay.imageloader.JImageLoader;
-import com.jay.imageloader.cache.DiskCacheStrategy;
-import com.jay.imageloader.config.RequestConfig;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ImageView imageView = (ImageView) findViewById(R.id.image);
-        RequestConfig config = new RequestConfig(this)
-                .cacheStrategy(DiskCacheStrategy.getInstance(getExternalCacheDir().getPath()))
-                .from("http://www.jikedaohang.com/images/first_code.jpg");
-        JImageLoader.displayImage(imageView, config);
     }
 }
